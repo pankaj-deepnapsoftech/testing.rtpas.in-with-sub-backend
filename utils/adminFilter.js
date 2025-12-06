2e7aa57d3afe1ca1bfaeb95190876b91140bf612
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-06 13:13:18 +0530

    updates in employee verified and admin creation bug

--- utils/adminFilter.js
+++ utils/adminFilter.js
@@ -1,7 +1,4 @@
-/**
- * Utility functions for admin data filtering
- * Ensures each admin only sees their own data unless they are a super admin
- */
+
 
 /**
  * Get admin filter for queries
@@ -9,20 +6,8 @@
  * @returns {Object} - MongoDB filter object
  */
 exports.getAdminFilter = (user) => {
-  // If user is super admin, return empty filter (can see all data)
-  // Note: You might want to add a check here if there's a distinction between
-  // super admin and regular admin. For now, we'll use isSuper flag.
   
-  // If user is not super admin, filter by their admin_id
-  // We'll use creator field or admin_id field depending on the model
-  if (user?.isSuper) {
-    // Super admin can see all data - return empty filter
-    return {};
-  }
-  
-  // Regular admin or employee - only see their own data
-  // Return filter by admin_id (which should be set to user._id for admins)
-  return { admin_id: user._id };
+  return { admin_id: user?._id };
 };
 
 /**
@@ -31,15 +16,12 @@ exports.getAdminFilter = (user) => {
  * @returns {ObjectId|null} - Admin ID to use, or null if super admin
  */
 exports.getAdminIdForCreation = (user) => {
-  // If user is super admin, they might not need admin_id
-  // But for consistency, we'll still set it
+  
   if (user?.isSuper) {
-    return user._id; // Set admin_id to the super admin's ID
+    return user._id; 
   }
   
-  // For regular admins and employees, set admin_id to their admin's ID
-  // If employee belongs to an admin, we might need to track that separately
-  // For now, we'll use the user's ID
+  
   return user._id;
 };
 
@@ -51,16 +33,8 @@ exports.getAdminIdForCreation = (user) => {
  * @returns {Boolean} - True if user can access the record
  */
 exports.canAccessRecord = (user, record, adminField = null) => {
-  // Super admin can access everything
-  if (user?.isSuper) {
-    return true;
-  }
-  
-  // Determine which field to check
   const fieldToCheck = adminField || record.admin_id ? 'admin_id' : 'creator';
   const recordAdminId = record[fieldToCheck];
-  
-  // User can access if the record belongs to them
-  return recordAdminId && recordAdminId.toString() === user._id.toString();
+  return recordAdminId && recordAdminId.toString() === user?._id?.toString();
 };
 
